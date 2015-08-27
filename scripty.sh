563ebbfc278509680990205e513fb93a7b6da028
Mohamed Mustafa <m.shico.o@gmail.com>
2015-08-27 14:13:11 +0200

    added fedora
    this script will install the most important apps for your Ubuntu/Mint and Fedora or any Distro based on

--- scripty.sh
+++ scripty.sh
@@ -1,8 +1,30 @@
-#This Script under GPLv3.0 license http://www.gnu.org/licenses/gpl-3.0.html
+#!/bin/bash
 
+#This Script under GPLv3.0 license http://www.gnu.org/licenses/gpl-3.0.html
+#Made by Muhammad Mustafa m.shico.o@gmail.com , updated by Mohamed Hamza "M.hamza20@icloud.com"
+
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+   
+elif [ -f /etc/debian_version ]; then
+    DISTRO= "Ubuntu"
+    # XXX or Debian
+elif [ -f /etc/redhat-release ]; then
+    DISTRO="Fedora"
+    # XXX or CentOS or Fedora
+else
+    DISTRO=$(uname -s)
+fi
+
+echo " your deistro is $DISTRO"
+
+if [ $DISTRO = Ubuntu ] ;
+then 
 
 #update Distro
 ##############
+echo " "
+echo "#########################"
 echo "i will update your DISTRO"
 echo "#########################"
 echo " "
@@ -12,6 +34,8 @@ sudo apt-get dist-upgrade -y
 ############################
 #add Repos
 ##########
+echo " "
+echo "#############################"
 echo "i will add cool repos for you"
 echo "#############################"
 echo " "
@@ -25,6 +49,7 @@ sudo add-apt-repository ppa:noobslab/apps -y
 ##########################################################
 #install Progs
 ##############
+echo "############################"
 echo "i will install progs for you"
 echo "############################"
 echo " "
@@ -81,6 +106,90 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
-echo "Congrats, All apps are now ready, installation successfull"
+echo " "
+echo "#########################################################"
+echo "Congrats, All apps are now ready, installation succesfull"
 echo "#########################################################"
 echo " "
+
+fi
+
+if [ $DISTRO = Fedora ] ;
+then 
+
+#update Distro
+##############
+echo " "
+echo "#########################"
+echo "i will update your DISTRO"
+echo "#########################"
+echo " "
+
+sudo dnf -y update 
+#add Repos
+##########
+echo " "
+echo "#############################"
+echo "i will add cool repos for you"
+echo "#############################"
+echo " "
+
+echo  "Install RPMFusion free & nonfree "
+dnf -y  install --nogpgcheck   http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+echo  "Install Google-chrome repo "
+
+wget https://dl-ssl.google.com/linux/linux_signing_key.pub
+rpm --import linux_signing_key.pub
+
+# Cleanup
+rm -f linux_signing_key.pub
+
+
+echo "[google-chrome]
+name=Google Chrome - \$basearch
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch" >> /etc/yum.repos.d/google-chrome.repo
+
+dnf -y update 
+
+##########################################################
+#install Progs
+##############
+echo " "
+echo "############################"
+echo "i will install progs for you"
+echo "############################"
+echo " "
+
+
+dnf -y install vlc
+dnf -y install gnome-tweak-tool
+dnf -y install google-chrome-stable
+dnf -y install nano
+dnf -y install vim
+dnf -y install gimp
+dnf -y install gparted
+
+
+echo " with this program named fedy u can install  mp3 codecs , skype ,Team viewer and more good stuffs "
+curl https://satya164.github.io/fedy/fedy-installer -o fedy-installer && chmod +x fedy-installer && ./fedy-installer
+
+dnf -y install java 
+dnf -y instrar p7zip
+dnf -y install gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1
+
+echo "[Dropbox]
+name=Dropbox Repository
+baseurl=http://linux.dropbox.com/fedora/20/
+gpgkey=http://linux.dropbox.com/fedora/rpm-public-key.asc">> /etc/yum.repos.d/dropbox.repo
+
+dnf -y install nautilus-dropbox
+dnf -y install nautilus-open-terminal
+
+echo "Congrats, All apps are now ready, installation succesfull"
+echo "#########################################################"
+echo " "
+fi
+
+
